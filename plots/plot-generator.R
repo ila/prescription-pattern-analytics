9cab96dc9e730480d5d2ca3172375f8af92c8700
Ila <ilaria.battiston@gmail.com>
2019-04-01 22:30:29 +0200

    aggiunta roba su r

--- plots/plot-generator.R
+++ plots/plot-generator.R
@@ -42,34 +42,76 @@ png(filename=paste(image_path, "top_atc-month.png", sep=""), width=2200, height=
 dev.off()
 
 
-# # rifaximina
-# r$mese <- as.Date(r$mese)
-# ggplot(r, aes(x=mese, y=totale)) + geom_point() + geom_line() + scale_x_date(labels=date_format("%y/%m"), breaks=date_breaks("6 months")) + scale_y_continuous(breaks=seq(0, 7500, by=500)) + labs(x="Mese", y="Totale prescrizioni")
-# 
-# # antibiotici classe A
-# topatc$mese <- as.Date(topatc$mese)
-# ggplot(topatc, aes(x=mese, y=totale, color=co_atc)) + geom_point() + geom_line() + scale_x_date(labels=date_format("%y/%m"), breaks=date_breaks("6 months")) + scale_y_continuous(breaks=seq(0, 30000, by=1000)) + labs(x="Mese", y="Totale prescrizioni", color="Codice ATC")
-# 
-# # codifa anno
-# codifa$co_codifa <- factor(codifa$co_codifa)
-# ggplot(codifa, aes(x=anno, y=count, color=co_codifa)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)) + scale_y_continuous(breaks = seq(0, 100000, by=5000)) + labs(x="Anno", y="Totale prescrizioni", color="Codice")
-# 
-# # codifa mese
-# codifa$co_codifa <- factor(codifa$co_codifa)
-# codifa$mese <- as.Date(codifa$mese)
-# ggplot(codifa, aes(x=mese, y=count, color=co_codifa)) + geom_point() + geom_line() + scale_x_date(labels=date_format("%y/%m"), breaks=date_breaks("6 months")) + scale_y_continuous(breaks=seq(0, 13000, by=1000)) + labs(x="Mese", y="Totale prescrizioni", color="Codice")
-# 
+# A class antibiotics trend by month
+atcamonth <- read.csv(paste(csv_path, "top_atc_a-month.csv", sep=""))
+
+# parsing to date and removing 2018 values [incomplete year]
+atcamonth$mese <- as.Date(atcamonth$mese)
+atcamonth <- atcamonth[which(atcamonth$mese <= as.Date("2018-06-01")),]
+
+# plotting and saving as .png
+png(filename=paste(image_path, "top_atc_a-month.png", sep=""), width=2200, height=1100, res=200)
+
+  atcamonthplot <- ggplot(atcamonth, aes(x=mese, y=totale, color=co_atc)) + geom_point() + geom_line() + scale_x_date(labels=date_format("%y/%m"), breaks=date_breaks("6 months")) + scale_y_continuous(breaks=seq(0, 30000, by=1000), labels=comma) + labs(x="Month", y="Total prescriptions") + scale_color_discrete(name="ATC code", labels=c("Nystatin", "Paromomycin", "Rifaximin"))
+
+  print(atcamonthplot)
+
+dev.off()
+
+
+# plotting AIC trends
+aicyear <- read.csv(paste(csv_path, "top_aic-year.csv", sep=""))
+
+# turning values to factors and removing 2018
+aicyear$co_codifa <- factor(aicyear$co_codifa)
+aicyear <- aicyear[which(aicyear$anno <= 2017),]
+
+png(filename=paste(image_path, "top_aic-year.png", sep=""), width=2200, height=1100, res=200)
+
+  aicyearplot <- ggplot(aicyear, aes(x=anno, y=count, color=co_codifa)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)) + scale_y_continuous(breaks = seq(0, 100000, by=5000), labels=comma) + labs(x="Year", y="Total prescriptions") + scale_color_discrete(name="AIC code", labels=c("Velamox", "Normix", "Monuril", "Augmentin", "Ciproxin", "Levoxacin"))
+
+  print(aicyearplot)
+
+dev.off()
+
+
+# AIC months
+aicmonth <- read.csv(paste(csv_path, "top_aic-month.csv", sep=""))
+
+aicmonth$co_codifa <- factor(aicmonth$co_codifa)
+aicmonth$mese <- as.Date(aicmonth$mese)
+aicmonth <- aicmonth[which(aicmonth$mese <= as.Date("2018-06-01")),]
+
+png(filename=paste(image_path, "top_aic-month.png", sep=""), width=2200, height=1100, res=200)
+
+  aicmonthplot <- ggplot(aicmonth, aes(x=mese, y=count, color=co_codifa)) + geom_point() + geom_line() + scale_x_date(labels=date_format("%y/%m"), breaks=date_breaks("6 months")) + scale_y_continuous(breaks=seq(0, 13000, by=1000), labels=comma) + labs(x="Month", y="Total prescriptions") + scale_color_discrete(name="AIC code", labels=c("Velamox", "Rocefin", "Normix", "Monuril", "Augmentin", "Ciproxin", "Zitromax", "Levoxacin"))
+  
+  print(aicmonthplot)
+  
+dev.off()
+
+
+# zooming on 4 AIC codes
+aic4 <- read.csv(paste(csv_path, "aic_4-year.csv", sep=""))
+
+# turning values to factors and removing 2018
+aic4$co_codifa <- factor(aic4$co_codifa)
+
+png(filename=paste(image_path, "aic_4-year.png", sep=""), width=2200, height=1100, res=200)
+
+aic4plot <- ggplot(aic4, aes(x=date_part, y=count, color=co_codifa)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)) + scale_y_continuous(breaks = seq(0, 100000, by=5000), labels=comma) + labs(x="Year", y="Total prescriptions") + scale_color_discrete(name="AIC code", labels=c("Velamox", "Normix", "Augmentin", "Levoxacin"))
+
+print(aic4plot)
+
+dev.off()
+
 # # codifa + atc
 # # trasformare a factor e date
 # codifa1 <- subset(codifa, co_codifa == "26089019")
 # topatc1 <- subset(topatc, co_atc == "J01CR02")
 # ggplot(topatc1, aes(x=mese, y=count)) + geom_line(color="cyan3") + geom_point(color="cyan3") + scale_y_continuous(breaks = seq(0, 25000, by=1000)) + geom_line(data=codifa1, aes(x=mese, y=count), color="blue2") + geom_point(data=codifa1, aes(x=mese, y=count), color="blue2") + scale_x_date(labels=date_format("%y/%m"), breaks=date_breaks("6 months"))
 # 
-# # test di correlazione codifa + atc
-# cor(topatc1$count, codifa1$count, method="pearson")
-# cor(topatc1$count, codifa1$count, method="kendall")
-# cor(topatc1$count, codifa1$count, method="spearman")
-# 
+
 # # donne
 # plot1 <- ggplot(donne, aes(x=anno, y=count, color=co_atc)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)) + scale_y_continuous(breaks = seq(0, 110000, by=5000), limits=c(0, 110000)) + labs(x="Anno", y="Totale prescrizioni", color="Codice ATC") + ggtitle("Donne")
 # plot2 <- ggplot(uomini, aes(x=anno, y=count, color=co_atc)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)) + scale_y_continuous(breaks = seq(0, 110000, by=5000), limits=c(0, 110000)) + labs(x="Anno", y="Totale prescrizioni", color="Codice ATC") + ggtitle("Uomini")
