dd7870ccd8db8ae973a735eb742cb1cdd0114ee8
Ila <ilaria.battiston@gmail.com>
2019-05-27 19:17:49 +0200

    tesi aggiornata con relativi grafici

--- plots/plot-generator.R
+++ plots/plot-generator.R
@@ -13,6 +13,18 @@ csv_path = "/Users/ila/Desktop/codes/cmr-internship/plots/csv/"
 image_path = "/Users/ila/Desktop/codes/cmr-internship/plots/"
 
 
+# plotting prescription numbers for years
+p <- read.csv(paste(csv_path, "yearly_prescriptions.csv", sep=""))
+
+png(filename=paste(image_path, "yearly_prescriptions.png", sep=""), width=1000, height=550, res=200)
+
+  pplot <- ggplot(p, aes(x=date_part, y=count)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017)) + scale_y_continuous(breaks = seq(0, 900000, by=50000), limits=c(700000, 900000), labels=comma) + labs(x="Year", y="Total prescriptions") 
+  
+  print(pplot)
+
+dev.off()
+
+
 # plotting ATC trends for years
 atcyear <- read.csv(paste(csv_path, "top_atc-year.csv", sep=""))
 
@@ -20,9 +32,9 @@ atcyear <- read.csv(paste(csv_path, "top_atc-year.csv", sep=""))
 atcyear <- atcyear[which(atcyear$anno <= 2017),]
 
 # plotting and saving as .png
-png(filename=paste(image_path, "top_atc-year.png", sep=""), width=2200, height=1100, res=200)
+png(filename=paste(image_path, "top_atc-year.png", sep=""), width=1500, height=600, res=200)
 
-  atcyearplot <- ggplot(atcyear, aes(x=anno, y=count, color=co_atc)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017)) + scale_y_continuous(breaks = seq(0, 200000, by=10000), labels=comma) + labs(x="Year", y="Total prescriptions", color="ATC code") + scale_color_discrete(name="ATC code", labels=c("Rifaximin", "Amoxicillin", "Amoxicillin and \nbeta-lactamase inhibitor", "Clarithromycin", "Ciprofloxacin", "Levofloxacin"))
+  atcyearplot <- ggplot(atcyear, aes(x=anno, y=count, color=co_atc)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017)) + scale_y_continuous(breaks = seq(0, 200000, by=15000), labels=comma) + labs(x="Year", y="Total prescriptions", color="ATC code") + scale_color_discrete(name="ATC code", labels=c("Rifaximin", "Amoxicillin", "Amoxicillin and \nbeta-lactamase inhibitor", "Clarithromycin", "Ciprofloxacin", "Levofloxacin"))
 
   print(atcyearplot)
 
@@ -37,9 +49,9 @@ atcmonth$mese <- as.Date(atcmonth$mese)
 atcmonth <- atcmonth[which(atcmonth$mese <= as.Date("2018-06-01")),]
 
 # plotting and saving as .png
-png(filename=paste(image_path, "top_atc-month.png", sep=""), width=2200, height=1100, res=200)
+png(filename=paste(image_path, "top_atc-month.png", sep=""), width=1500, height=600, res=200)
 
-  atcmonthplot <- ggplot(atcmonth, aes(x=mese, y=count, color=co_atc)) + geom_point() + geom_line() + scale_x_date(labels=date_format("%y/%m"), breaks=date_breaks("6 months")) + scale_y_continuous(breaks=seq(0, 30000, by=1000), labels=comma) + labs(x="Month", y="Total prescriptions") + scale_color_discrete(name="ATC code", labels=c("Rifaximin", "Amoxicillin", "Amoxicillin and \nbeta-lactamase inhibitor", "Ceftriaxone", "Clarithromycin", "Ciprofloxacin", "Levofloxacin"))
+  atcmonthplot <- ggplot(atcmonth, aes(x=mese, y=count, color=co_atc)) + geom_point() + geom_line() + scale_x_date(labels=date_format("%y/%m"), breaks=date_breaks("1 year")) + scale_y_continuous(breaks=seq(0, 30000, by=2000), labels=comma) + labs(x="Month", y="Total prescriptions") + scale_color_discrete(name="ATC code", labels=c("Rifaximin", "Amoxicillin", "Amoxicillin and \nbeta-lactamase inhibitor", "Ceftriaxone", "Clarithromycin", "Ciprofloxacin", "Levofloxacin"))
 
   print(atcmonthplot)
 
@@ -54,9 +66,9 @@ atcamonth$mese <- as.Date(atcamonth$mese)
 atcamonth <- atcamonth[which(atcamonth$mese <= as.Date("2018-06-01")),]
 
 # plotting and saving as .png
-png(filename=paste(image_path, "top_atc_a-month.png", sep=""), width=2200, height=1100, res=200)
+png(filename=paste(image_path, "top_atc_a-month.png", sep=""), width=1500, height=600, res=200)
 
-  atcamonthplot <- ggplot(atcamonth, aes(x=mese, y=totale, color=co_atc)) + geom_point() + geom_line() + scale_x_date(labels=date_format("%y/%m"), breaks=date_breaks("6 months")) + scale_y_continuous(breaks=seq(0, 30000, by=1000), labels=comma) + labs(x="Month", y="Total prescriptions") + scale_color_discrete(name="ATC code", labels=c("Nystatin", "Paromomycin", "Rifaximin"))
+  atcamonthplot <- ggplot(atcamonth, aes(x=mese, y=totale, color=co_atc)) + geom_point() + geom_line() + scale_x_date(labels=date_format("%y/%m"), breaks=date_breaks("1 year")) + scale_y_continuous(breaks=seq(0, 30000, by=1000), labels=comma) + labs(x="Month", y="Total prescriptions") + scale_color_discrete(name="ATC code", labels=c("Nystatin", "Paromomycin", "Rifaximin"))
 
   print(atcamonthplot)
 
@@ -70,9 +82,9 @@ aicyear <- read.csv(paste(csv_path, "top_aic-year.csv", sep=""))
 aicyear$co_codifa <- factor(aicyear$co_codifa)
 aicyear <- aicyear[which(aicyear$anno <= 2017),]
 
-png(filename=paste(image_path, "top_aic-year.png", sep=""), width=2200, height=1100, res=200)
+png(filename=paste(image_path, "top_aic-year.png", sep=""), width=1500, height=600, res=200)
 
-  aicyearplot <- ggplot(aicyear, aes(x=anno, y=count, color=co_codifa)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)) + scale_y_continuous(breaks = seq(0, 100000, by=5000), labels=comma) + labs(x="Year", y="Total prescriptions") + scale_color_discrete(name="AIC code", labels=c("Velamox", "Normix", "Monuril", "Augmentin", "Ciproxin", "Levoxacin"))
+  aicyearplot <- ggplot(aicyear, aes(x=anno, y=count, color=co_codifa)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)) + scale_y_continuous(breaks = seq(0, 100000, by=10000), labels=comma) + labs(x="Year", y="Total prescriptions") + scale_color_discrete(name="AIC code", labels=c("Velamox", "Normix", "Monuril", "Augmentin", "Ciproxin", "Levoxacin"))
 
   print(aicyearplot)
 
@@ -86,9 +98,9 @@ aicmonth$co_codifa <- factor(aicmonth$co_codifa)
 aicmonth$mese <- as.Date(aicmonth$mese)
 aicmonth <- aicmonth[which(aicmonth$mese <= as.Date("2018-06-01")),]
 
-png(filename=paste(image_path, "top_aic-month.png", sep=""), width=2200, height=1100, res=200)
+png(filename=paste(image_path, "top_aic-month.png", sep=""), width=1500, height=600, res=200)
 
-  aicmonthplot <- ggplot(aicmonth, aes(x=mese, y=count, color=co_codifa)) + geom_point() + geom_line() + scale_x_date(labels=date_format("%y/%m"), breaks=date_breaks("6 months")) + scale_y_continuous(breaks=seq(0, 13000, by=1000), labels=comma) + labs(x="Month", y="Total prescriptions") + scale_color_discrete(name="AIC code", labels=c("Velamox", "Rocefin", "Normix", "Monuril", "Augmentin", "Ciproxin", "Zitromax", "Levoxacin"))
+  aicmonthplot <- ggplot(aicmonth, aes(x=mese, y=count, color=co_codifa)) + geom_point() + geom_line() + scale_x_date(labels=date_format("%y/%m"), breaks=date_breaks("1 year")) + scale_y_continuous(breaks=seq(0, 13000, by=1000), labels=comma) + labs(x="Month", y="Total prescriptions") + scale_color_discrete(name="AIC code", labels=c("Velamox", "Rocefin", "Normix", "Monuril", "Augmentin", "Ciproxin", "Zitromax", "Levoxacin"))
   
   print(aicmonthplot)
   
@@ -128,10 +140,12 @@ dev.off()
 # zooming on top AIC of a subset of patients
 topaicsubset <- read.csv(paste(csv_path, "top_aic_subset-year.csv", sep=""))
 
+topaicsubset <- topaicsubset[which(topaicsubset$date_part <= 2017),]
+
 # turning values to factors and removing 2018
 topaicsubset$co_codifa <- factor(topaicsubset$co_codifa)
 
-png(filename=paste(image_path, "top_aic_subset-year.png", sep=""), width=2200, height=1100, res=200)
+png(filename=paste(image_path, "top_aic_subset-year.png", sep=""), width=1500, height=600, res=200)
 
   topaicsubsetplot <- ggplot(topaicsubset, aes(x=date_part, y=count, color=co_codifa)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)) + scale_y_continuous(breaks = seq(0, 100000, by=5000), labels=comma) + labs(x="Year", y="Total prescriptions") + scale_color_discrete(name="AIC code", labels=c("Velamox", "Normix", "Monuril", "Augmentin", "Ciproxin", "Levoxacin"))
 
@@ -161,7 +175,7 @@ dev.off()
 velamox <- read.csv(paste(csv_path, "velamox-year.csv", sep=""))
 
 # plotting and saving as .png
-png(filename=paste(image_path, "velamox-year.png", sep=""), width=2200, height=1100, res=200)
+png(filename=paste(image_path, "velamox-year.png", sep=""), width=1500, height=600, res=200)
 
   velamoxplot <- ggplot(velamox, aes(x=anno, y=count)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017)) + scale_y_continuous(breaks = seq(0, 200000, by=5000), labels=comma) + labs(x="Year", y="Total prescriptions") 
 
@@ -267,7 +281,7 @@ dev.off()
 augmentin <- read.csv(paste(csv_path, "augmentin-year.csv", sep=""))
 
 # plotting and saving as .png
-png(filename=paste(image_path, "augmentin-year.png", sep=""), width=2200, height=1100, res=200)
+png(filename=paste(image_path, "augmentin-year.png", sep=""), width=1500, height=600, res=200)
 
   augmentinplot <- ggplot(augmentin, aes(x=anno, y=count)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017)) + scale_y_continuous(breaks = seq(0, 200000, by=5000), labels=comma) + labs(x="Year", y="Total prescriptions") 
 
@@ -279,7 +293,7 @@ dev.off()
 # trends for age
 augmentinageyear$range = as.factor(augmentinageyear$range)
 
-png(filename=paste(image_path, "augmentin_age-year.png", sep=""), width=2200, height=1100, res=200)
+png(filename=paste(image_path, "augmentin_age-year.png", sep=""), width=1500, height=600, res=200)
 
   augmentinageplot <- ggplot(augmentinageyear, aes(x=year, y=count, colour=range)) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017)) + scale_y_continuous(breaks = seq(0, 200000, by=5000), labels=comma) + labs(x="Year", y="Total prescriptions") + scale_color_discrete(name="Age range", label=c("15-24", "25-44", "45-64", "65+"))
 
@@ -332,7 +346,7 @@ provinces$provincia = factor(provinces$provincia)
 
 png(filename=paste(image_path, "provinces.png", sep=""), width=2200, height=1100, res=200)
 
-  provincesplot <- ggplot(provinces, aes(x=anno, y=count, color=provincia)) + scale_color_discrete(name="Province", breaks=c(61, 62, 63, 64, 65), labels=c("Caserta", "Benevento", "Napoli", "Avellino", "Salerno")) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)) + scale_y_continuous(breaks = seq(0, 1000000, by=50000), labels=comma) + labs(x="Year", y="Total prescriptions") 
+  provincesplot <- ggplot(provinces, aes(x=anno, y=count, color=provincia)) + scale_color_discrete(name="Province", breaks=c(61, 62, 63, 64, 65), labels=c("Caserta", "Benevento", "Napoli", "Avellino", "Salerno")) + geom_point() + geom_line() + scale_x_continuous(breaks=c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)) + scale_y_continuous(breaks=seq(0, 1000000, by=50000), labels=comma) + labs(x="Year", y="Total prescriptions") 
   
   print(provincesplot)
 
@@ -346,14 +360,14 @@ plots_year <- list()
 valuesyear <- c()
 
 for (year in 2008:2017) {
-  plots_year[[length(plots_year) + 1]] <- ggplot(pnyear[which(pnyear$anno == year),], aes(x=factor(np), y=count)) + geom_bar(stat="identity") + labs(x="Number of prescriptions", y="Number of patients") + ggtitle(year) + scale_y_log10(limits=c(1,1e5), labels=comma)
+  plots_year[[length(plots_year) + 1]] <- ggplot(pnyear[which(pnyear$anno == year),], aes(x=factor(np), y=count)) + geom_bar(stat="identity") + labs(x="Number of prescriptions", y="Number of patients") + ggtitle(year) + scale_y_log10(limits=c(1,1e5), labels=comma) + scale_x_discrete(breaks = seq(0, 90, by=5))
   
   valuesyear <- c(valuesyear, pnyear[which(pnyear$anno == year & pnyear$np == 1),]$count)
 }
 
 statsyear <- c(mean(valuesyear), var(valuesyear), sd(valuesyear))
 
-png(filename=paste(image_path, "prescriptions_number-year.png", sep=""), width=8000, height=8000, res=300)
+png(filename=paste(image_path, "prescriptions_number-year.png", sep=""), width=3000, height=5000, res=300)
 
   print(do.call("grid.arrange", c(plots_year, ncol=2)))
 
@@ -372,7 +386,7 @@ valuesmonth <- c()
 for (month in 1:12) {
   plot <- pnmonth2017[which(month(pnmonth2017$data) == month),]
   
-  plots_month[[length(plots_month) + 1]] <- ggplot(plot, aes(x=factor(np), y=count)) + geom_bar(stat="identity") + geom_vline(aes(xintercept=mean(count))) + labs(x="Number of prescriptions", y="Number of patients") + ggtitle(month) + scale_y_log10(limits=c(1,1e5), labels=comma)
+  plots_month[[length(plots_month) + 1]] <- ggplot(plot, aes(x=factor(np), y=count)) + geom_bar(stat="identity") + geom_vline(aes(xintercept=mean(count))) + labs(x="Number of prescriptions", y="Number of patients") + ggtitle(paste("Month ", month, sep="")) + scale_y_log10(limits=c(1,1e5), labels=comma)
   
   valuesmonth <- c(valuesmonth, plot[which(plot$np == 1),]$count)
   
@@ -380,7 +394,7 @@ for (month in 1:12) {
 
 statsmonth <- c(mean(valuesmonth), var(valuesmonth), sd(valuesmonth))
 
-png(filename=paste(image_path, "prescriptions_number-month.png", sep=""), width=5000, height=8000, res=300)
+png(filename=paste(image_path, "prescriptions_number-month.png", sep=""), width=3000, height=4000, res=300)
 
   print(do.call("grid.arrange", c(plots_month, ncol=2)))
 
